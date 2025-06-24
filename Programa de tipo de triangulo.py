9406b5e210511b8898f7b94dcdf6d85c178bbd10
Noemi1606 <2025-3601@unad.edu.do>
2025-06-24 00:12:50 -0400

    Ejercicio de tipo de triangulo

---
+++ Programa de tipo de triangulo.py
@@ -0,0 +1,10 @@
+# Pide tres longitudes y determina si el triángulo es equilátero, isósceles o escaleno.
+lado1 = float(input("Ingresa la longitud del primer lado: "))
+lado2 = float(input("Ingresa la longitud del segundo lado: "))
+lado3 = float(input("Ingresa la longitud del tercer lado: "))
+
+if lado1 + lado2 > lado3 and lado1 + lado3 > lado2 and lado2 + lado3 > lado1:
+    if lado1 == lado2 == lado3:
+        print("Es un triángulo equilátero.")
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        p
