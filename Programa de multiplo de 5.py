3bd888757ec0869679fac46508eca215af4bf7dd
Noemi1606 <2025-3601@unad.edu.do>
2025-06-23 23:09:04 -0400

    Ejercicio múltiplo de 5

---
+++ Programa de multiplo de 5.py
@@ -0,0 +1,7 @@
+#Escribe un programa que pida un número y determine si es múltiplo de 5.
+numero = int(input("Ingresa un número: "))
+
+if numero % 5 == 0:
+    print("El número es múltiplo de 5.")
+else:
+    print("El número no es múltiplo de 5.")
