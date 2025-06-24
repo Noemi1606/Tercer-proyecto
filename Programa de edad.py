320d47b55b36b6f5602e8da6f7d914120c4a9de9
Noemi1606 <2025-3601@unad.edu.do>
2025-06-23 23:04:22 -0400

    Ejercicio de edad

---
+++ Programa de edad.py
@@ -0,0 +1,7 @@
+#Solicita la edad de una persona e imprime si es mayor o menor de edad (mayor de 18 años).
+edad = int(input("Ingresa tu edad: "))
+
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
