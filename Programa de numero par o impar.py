6abc1e3c52780003520c3fbbe3b1fba3a0ce6631
Noemi1606 <2025-3601@unad.edu.do>
2025-06-23 23:00:04 -0400

    Ejercicio de número par o impar

---
+++ Programa de numero par o impar.py
@@ -0,0 +1,7 @@
+#Pide al usuario que ingrese un número entero y determina si es par o impar.
+numero = int(input("Ingresa un número entero: "))
+
+if numero % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
