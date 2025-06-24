28e57167fc09e9860164a20d97ef65a11b9fd3b0
Noemi1606 <2025-3601@unad.edu.do>
2025-06-23 23:12:35 -0400

    Descuento por edad

---
+++ Programa de descuento por edad.py
@@ -0,0 +1,7 @@
+#Un cine ofrece un descuento del 50% a personas mayores de 60 años. Solicita la edad del usuario y determina si aplica para el descuento.
+edad = int(input("Ingresa tu edad: "))
+
+if edad >= 60:
+    print("Aplicas para el descuento del 50%.")
+else:
+    print("No aplicas para el descuento.")
